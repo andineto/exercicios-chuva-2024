bcbb80c0c9a8b36645fdaac5f1e3779dfeea4a70
andineto <122061209+andineto@users.noreply.github.com>
2024-04-04 16:40:57 -0300

    feat: loop por elementos e teste se é paper
    Loop por cada elemento com tag 'a' e testa se o elemento atual é um paper usando o getAttribute('class')

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,18 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+    $papersArray = array();
+    // Loop através de cada tag "a"
+    $elements = $dom->getElementsByTagName('a');
 
+    foreach ($elements as $element) 
+    {
+        // Verificar se o elemento possui a classe "paper-card p-lg bd-gradient-left"
+        if ($element->getAttribute('class') == 'paper-card p-lg bd-gradient-left')
+        {
+
+        }
+    }
+
+  }
 }
